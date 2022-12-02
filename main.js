efaf52ad39f036ebd0a65ae9071d2fd302fd90ba
unknown <antonio.cortes.sotelo@gmail.com>
2022-12-02 15:30:15 -0300

    add

--- main.js
+++ main.js
@@ -1,152 +1,189 @@
-const generar_tabla = () => {
-  // Obtenemos los valores ingresados desde el modal.
-  var fabricas = parseInt(document.getElementById('xfabrica').value);
-  var bodegas = parseInt(document.getElementById('ybodega').value);
-
-  const abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-  // Validamos que estos valores esten dentro de los parametros definidos.
-  if (fabricas < 2 || fabricas > 8 || bodegas < 2 || bodegas > 8) {
-    alert('Los valores ingresados no estan dentro de los parametros.');
-    location.reload()
-  } else {
-    //obtenemos y limpiamos la tabla.
-    var tabla = document.getElementById('tabla');
-    tabla.innerHTML = ''
-    // creamos el cuerpo de la tabla con su hilera principal.
-    var cuerpo = document.createElement('tbody');
-    var hileraFabricas = document.createElement('tr');
-    for (let i = 0; i < fabricas; i++) {
-      if (i == 0) {
-        var th_hilera = document.createElement('th');
-        th_hilera.innerHTML = 'Fabrica ' + abc[i];
-        th_hilera.colSpan = '2';
-        hileraFabricas.appendChild(th_hilera)
-      } else {
-        var th_hilera = document.createElement('th');
-        th_hilera.innerHTML = 'Fabrica ' + abc[i];
-        hileraFabricas.appendChild(th_hilera)
-      }
-    } cuerpo.appendChild(hileraFabricas);
-    //creamos las hileras de bodegas.
-
-    for (let z = 0; z < bodegas; z++) {
-      var hilerabodega = document.createElement('tr');
-      for (let x = 0; x <= fabricas +1; x++) {
-
-
-        if (x == 0) {
-          var thHilera = document.createElement('th');
-          thHilera.innerHTML = 'Bodega ' + (z + 1);
-          hilerabodega.appendChild(thHilera);
-        }
-
-        else {
-          if (x == (fabricas + 1)) {
-            var thHilera = document.createElement('td');
-            thHilera.innerHTML = `z - ${z}. x- ${x}  `;
-            thHilera.className += 'recuadros'
-            hilerabodega.appendChild(thHilera);
-          } else {
-            var thHilera = document.createElement('td');
-            thHilera.innerHTML = `<input placeholder="$" min=0 value="${z}" type="number"> <input placeholder="C" value=${x-1} min=0 type="number">`;
-            thHilera.className += 'recuadros'
-            hilerabodega.appendChild(thHilera);
-          }
-        }
-
-
-      }
-      cuerpo.appendChild(hilerabodega);
-    };
-
-    var hileraULT = document.createElement('tr');
-    hileraULT.className+= 'ultimaHilera'
-    for (let i = 0; i < fabricas; i++) {
-      if (i == 0) {
-        var th_hilera = document.createElement('th');
-        th_hilera.innerHTML = `${i}`;
-        th_hilera.colSpan = '2';
-        hileraULT.appendChild(th_hilera)
-      } else {
-        var th_hilera = document.createElement('th');
-        th_hilera.innerHTML = `${i}`;
-
-        hileraULT.appendChild(th_hilera)
-      }
-    } cuerpo.appendChild(hileraULT);
-
-
-
-    tabla.appendChild(cuerpo);
-    const modal = document.getElementById('contenerdormodal');
-    modal.style.display = 'none';
+function generaTabla(){
+  var fuentes = parseInt(document.getElementById('fil').value);
+  var destinos = parseInt(document.getElementById('col').value);
+  var lol = document.getElementById("tabla")
+  var body = document.getElementsByTagName("body")[0];
+  var d=1;
+  var f=1;
+  if(lol){
+    lol.parentNode.removeChild(lol);
   }
-
-};
-
-const tabla_aleatoria = () => {
-  var min = 2;
-  var max = 8;
-  var fabricas = 2;
-  var bodegas = 2;
-  // Obtenemos los valores ingresados desde el modal
-  const abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-  // Validamos que estos valores esten dentro de los parametros definidos.
-  if (fabricas < 2 || fabricas > 8 || bodegas < 2 || bodegas > 8) {
-    alert('Los valores ingresados no estan dentro de los parametros.');
-    location.reload()
-  } else {
-    //obtenemos y limpiamos la tabla.
-    var tabla = document.getElementById('tabla');
-    tabla.innerHTML = ''
-    // creamos el cuerpo de la tabla con su hilera principal.
-    var cuerpo = document.createElement('tbody');
-    var hileraFabricas = document.createElement('tr');
-    for (let i = 0; i < fabricas; i++) {
-      if (i == 0) {
-        var th_hilera = document.createElement('th');
-        th_hilera.innerHTML = 'Fabrica ' + abc[i];
-        th_hilera.colSpan = '2';
-        hileraFabricas.appendChild(th_hilera)
-      } else {
-        var th_hilera = document.createElement('th');
-        th_hilera.innerHTML = 'Fabrica ' + abc[i];
-        hileraFabricas.appendChild(th_hilera)
+  var formu   = document.createElement("form");
+  formu.setAttribute("id","tabla");
+  var tabla   = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+  for (var i = 0; i <= fuentes+1; i++) {
+    var filas = document.createElement("tr");
+    for (var j = 0; j <= destinos+1; j++) {
+      var columnas = document.createElement("td");
+      if(i==0 && j>0 && j<=destinos){
+        var textoCelda1= document.createTextNode("Dest".concat(d.toString()));
+        d++;
+        columnas.appendChild(textoCelda1);
+      } else if( j==0 && i > 0 && i<=fuentes){
+        var textoCelda2= document.createTextNode("Fuente".concat(f.toString()));
+        f++;
+        columnas.appendChild(textoCelda2);
+      }else if( j>0 && i > 0 && (i!=fuentes+1 || j!=destinos+1)){
+        var textoCelda3 = document.createElement("input");
+        textoCelda3.setAttribute("id","t".concat(i.toString(),"_",j.toString()));
+        textoCelda3.setAttribute("width","5px");
+        columnas.appendChild(textoCelda3);
       }
-    } cuerpo.appendChild(hileraFabricas);
-    //creamos las hileras de bodegas.
-
-    for (let z = 0; z < bodegas; z++) {
-      var hilerabodega = document.createElement('tr');
-      for (let x = 0; x <= fabricas; x++) {
-        if (x == 0) {
-          var thHilera = document.createElement('th');
-          thHilera.innerHTML = 'Bodega ' + (z + 1);
-          hilerabodega.appendChild(thHilera);
-        } else {
-          var thHilera = document.createElement('td');
-          thHilera.innerHTML = '<input placeholder="$" min=0 type="number"> <input placeholder="C" min=0 type="number">';
-          thHilera.className += 'recuadros'
-          hilerabodega.appendChild(thHilera);
-        }
-      }
-      cuerpo.appendChild(hilerabodega);
+      filas.appendChild(columnas);
     }
-    tabla.appendChild(cuerpo);
-    const modal = document.getElementById('contenerdormodal');
-    modal.style.display = 'none';
-  };
-};
+    tblBody.appendChild(filas);
+  }
+  var boton = document.createElement("input");
+  boton.setAttribute("id","datos_table");
+  boton.setAttribute("value","Resolver");
+  boton.setAttribute("type","button");
+  boton.setAttribute("onclick","resuelve()");
+  tabla.appendChild(tblBody);
+  formu.appendChild(tabla);
+  formu.appendChild(boton);
+  body.appendChild(formu);
+  tabla.setAttribute("border", "2");
+  document.getElementById("form1").style.display="none";
+  document.getElementById("datos_table").style.display="block";
+  document.getElementById("repito").style.display="block";
+}
+function atras(){
+  document.getElementById("form1").style.display="block";
+  document.getElementById("datos_table").style.display="none";
+  document.getElementById("repito").style.display="none";
+  var lol = document.getElementById("tabla")
+  if(lol){
+    lol.parentNode.removeChild(lol);
+  }
+  var lol1 = document.getElementById("tablas")
+  if(lol1){
+    lol1.parentNode.removeChild(lol1);
+  }
+}
 
+function resuelve() {
+  var tabla=new Array();
+  var fuentes = parseInt(document.getElementById('fil').value);
+  var destinos = parseInt(document.getElementById('col').value);
+  for (var i = 1; i <= fuentes+1; i++) {
+    var col=new Array();
+    for (var j = 1; j <= destinos+1; j++) {
+      if(i<=fuentes || j <= destinos){
+        col[j-1]=parseInt(document.getElementById("t".concat(i.toString(),"_",j.toString())).value);
+      }
+    }
+    tabla[i-1] = col;
+  }
+  var resultado=algoritmoEsquina(tabla);
+  alert("El resultado es: "+ resultado);
+}
+function algoritmoEsquina(tabla) {
+  var res = new Array();
+  var res1 = new Array();
+  var res2 = new Array();
+  var resultado = 0;
+  var es = 0;
+  var dem = 0;
+  var offer = 0;
+  var i = 0;
+  var j = 0;
+  var lol = document.getElementById("tabla1")
+  var body = document.getElementsByTagName("body")[0];
 
+  while(tabla.length>1){
+    es=tabla[i][i];
+    dem=tabla[i][tabla[i].length-1];
+    offer=tabla[tabla.length-1][i];
+    if(dem>offer){
+      res1[j]=es;
+      res2[j]=offer;
+      res[j]=es*offer;
+      tabla[i][tabla[i].length-1]-=offer;
+      for (var z = 0; z < tabla.length; z++) {
+        tabla[z].splice(0,1);
+      }
+      console.log(tabla);
+    } else if (dem < offer){
+      res1[j]=es;
+      res2[j]=dem;
+      res[j]=es*dem;
+      tabla[tabla.length-1][i]-=dem;
+      tabla.splice(0,1);
+      console.log(tabla);
+    } else{
+      res[j]=es*dem;
+      res1[j]=es;
+      res2[j]=dem;
+      tabla.splice(0,1);
+      for (var z = 0; z < tabla.length; z++) {
+        tabla[z].splice(0,1);
+      }
+      console.log(tabla);
+    }
+    j++;
+  }
+  console.log(res);
+  for (var k = 0; k < res.length; k++) {
+    resultado+=res[k];
+  }
+  var lol = document.getElementById("tablas")
+  if(lol){
+    lol.parentNode.removeChild(lol);
+  }
+  var final=new Array();
+  final[0]=res1;
+  final[1]=res2;
+  final[2]=res;
+  //-----------------------------------------------------------------
+  var body = document.getElementsByTagName("body")[0];
 
-const newTable = () => {
-  // Manipulacion de modal para editar tabla actual.
-  var modal = document.getElementById('contenerdormodal');
-  modal.style.display = 'flex';
-};
-const closeModal = () => {
-  //manipulacion de modal para ocultar display.
-  var modal = document.getElementById('contenerdormodal');
-  modal.style.display = 'none';
-};
+ // Crea un elemento <table> y un elemento <tbody>
+ var tabla   = document.createElement("table");
+ var tblBody = document.createElement("tbody");
+tabla.setAttribute("id","tablas");
+
+ // Crea las celdas
+ for (var i = 0; i < res1.length; i++) {
+   // Crea las hileras de la tabla
+   var hilera = document.createElement("tr");
+
+   for (var j = 0; j < 3; j++) {
+     // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+     // texto sea el contenido de <td>, ubica el elemento <td> al final
+     // de la hilera de la tabla
+     var celda = document.createElement("td");
+     var textoCelda = document.createTextNode(final[j][i]);
+     celda.appendChild(textoCelda);
+     hilera.appendChild(celda);
+   }
+
+   // agrega la hilera al final de la tabla (al final del elemento tblbody)
+   tblBody.appendChild(hilera);
+ }
+ var hilera = document.createElement("tr");
+ var celda = document.createElement("td");
+ var textoCelda = document.createTextNode('');
+ celda.appendChild(textoCelda);
+ hilera.appendChild(celda);
+ tblBody.appendChild(hilera);
+ tabla.appendChild(tblBody);
+ var celda = document.createElement("td");
+ var textoCelda = document.createTextNode('');
+ celda.appendChild(textoCelda);
+ hilera.appendChild(celda);
+ tblBody.appendChild(hilera);
+ tabla.appendChild(tblBody);
+ var celda = document.createElement("td");
+ var textoCelda = document.createTextNode(resultado);
+ celda.appendChild(textoCelda);
+ hilera.appendChild(celda);
+ tblBody.appendChild(hilera);
+ tabla.appendChild(tblBody);
+ // appends <table> into <body>
+ body.appendChild(tabla);
+ // modifica el atributo "border" de la tabla y lo fija a "2";
+ tabla.setAttribute("border", "2");
+  //------------------------------------------------------------------
+  return resultado;
+}
